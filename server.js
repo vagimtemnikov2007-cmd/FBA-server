3e1911f148022f0469e4bf43be525323bd581e7d
Denis Temnikov <vagim.temnikov.2007@gmail.com>
2026-09-13 13:34:43 +0500

    rereadd visitors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-app.post("/api/telemetry/visitors", async (req, res) => {
+app.post("/visitors", async (req, res) => {
   const { visitorId } = req.body;
   if (!visitorId) {
     return res.status(400).json({ error: "visitorId is required" });
